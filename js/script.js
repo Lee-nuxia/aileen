05fc70a7200b679f18a9f5f71ea3d9bab8255c11
Aileen <airich091594@gmail.com>
2021-05-12 15:58:37 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
 	});
 
 	var typed = new Typed(".typed", {
-		strings: ["Graphic Designer.", "Website Developer.", "IT Technician."],
+		strings: ["Graphic Designer.", "Web Developer.", "IT Technician."],
 		typeSpeed: 70,
 		loop: true,
 		startDelay: 1000,
